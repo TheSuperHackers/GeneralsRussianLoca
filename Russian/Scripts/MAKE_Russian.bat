0e8c7318df5087448ca2db7a4f767330c9898f6d
xezon <4720891+xezon@users.noreply.github.com>
2021-09-25 10:38:58 +0200

    Fix Russian.big and RussianZH.big contents generated by scripts

--- Russian/Scripts/MAKE_Russian.bat
+++ Russian/Scripts/MAKE_Russian.bat
@@ -5,15 +5,15 @@ call %ThisDir1%SETUP_Folders.bat
 set BigName=820_Russian
 
 :: Free folders of big file contents
-del /s /f /q %GeneratedBigFilesUnpackedDir%\%BigName%
-del /s /f /q %GeneratedBigFilesDir%\%BigName%.big
+del /f /q /s %GeneratedBigFilesUnpackedDir%\%BigName%
+del /f /q    %GeneratedBigFilesDir%\%BigName%.big
 
 :: Copy .big contents
-xcopy /Y %GameFilesDir%\Data\Russian\*.* %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Russian\
-xcopy /Y %GameFilesDir%\Data\Russian\Art\Textures\*.* %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Russian\
+xcopy /y %GameFilesDir%\Data\Russian\*.* %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Russian\
+xcopy /y %GameFilesDir%\Data\Russian\Art\Textures\*.* %GeneratedBigFilesUnpackedDir%\%BigName%\Data\Russian\Art\Textures\
 
 :: Generate .big file(s)
 %ToolsDir%\GeneralsBigCreator\GeneralsBigCreator.exe -source %GeneratedBigFilesUnpackedDir%\%BigName% -dest %GeneratedBigFilesDir%\%BigName%.big
 
 :: Generate Release file(s)
-xcopy /Y %GeneratedBigFilesDir%\%BigName%.big %GeneratedReleaseUnpackedDir%\%BigName%.big*
+xcopy /y %GeneratedBigFilesDir%\%BigName%.big %GeneratedReleaseUnpackedDir%\%BigName%.big*
